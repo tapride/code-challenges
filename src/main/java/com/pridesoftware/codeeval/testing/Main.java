799000296ea1b391bdb6a2a4a356fba48482aaf7
Tom Pride <PrideThomasA@PrideFamily.us>
2017-08-11 22:11:27 -0500

    Added Main, MainTest, and input.txt
    Added Main & MainTest class frameworks.
    Added input.txt for testing

---
+++ src/main/java/com/pridesoftware/codeeval/testing/Main.java
@@ -0,0 +1,64 @@
+package com.pridesoftware.codeeval.testing;
+
+/**
+ * TESTING
+ *
+ * CHALLENGE DESCRIPTION:
+ * In many teams, there is a person who tests a project, finds bugs and errors, and prioritizes them. Now, you have
+ * the unique opportunity to try yourself as a tester and test a product. Here, you have two strings - the first one
+ * is provided by developers, and the second one is mentioned in design. You have to find and count the number of bugs,
+ * and also prioritize them for fixing using the following statuses: 'Low', 'Medium', 'High', 'Critical' or 'Done'.
+ *
+ * INPUT SAMPLE:
+ * The first argument is a path to a file. Each line includes a test case with two strings separated by a pipeline '|'.
+ * The first string is the one the developers provided to you for testing, and the second one is from design.
+ *
+ * Heelo Codevval | Hello Codeeval
+ * hELLO cODEEVAL | Hello Codeeval
+ * Hello Codeeval | Hello Codeeval
+ *
+ * OUTPUT SAMPLE:
+ * Write a program that counts the number of bugs and prioritizes them for fixing using the following statuses:
+ * 'Low' - 2 or fewer bugs;
+ * 'Medium' - 4 or fewer bugs;
+ * 'High' - 6 or fewer bugs;
+ * 'Critical' - more than 6 bugs;
+ * 'Done' - all is done;
+ *
+ * Low
+ * Critical
+ * Done
+ *
+ * CONSTRAINTS:
+ * Strings are of the same length from 5 to 40 characters.
+ * Upper and lower case matters.
+ * The number of test cases is 40.
+ */
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Main {
+
+    public static void main(String[] args){
+        String fileName = args[0];
+
+        try(BufferedReader bufferedReader = new BufferedReader(new FileReader(fileName))) {
+            String line;
+            while((line = bufferedReader.readLine()) != null){
+                String sentence = processLine(line);
+                System.out.println(sentence);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+
+        }
+
+    }
+
+    public static String processLine(String line){
+        return line;
+    }
+}
---
+++ src/test/java/com/pridesoftware/codeeval/testing/MainTest.java
@@ -0,0 +1,42 @@
+package com.pridesoftware.codeeval.testing;
+
+import com.pridesoftware.codeeval.lowercase.Main;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+public class MainTest {
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private final ByteArrayOutputStream errContent = new ByteArrayOutputStream();
+
+    @Before
+    public void setUp() throws Exception {
+        System.setOut(new PrintStream(outContent));
+        System.setErr(new PrintStream(errContent));
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        System.setOut(null);
+        System.setErr(null);
+    }
+
+    @Test
+    public void testMain() throws Exception {
+        String[] args = new String[]{"input/Testing/input.txt"};
+
+        String expected =
+                "Low" + System.getProperty("line.separator") +
+                "Critical" + System.getProperty("line.separator") +
+                "Done" + System.getProperty("line.separator");
+
+        Main.main(args);
+        String actual = outContent.toString();
+        Assert.assertEquals(expected, actual);
+
+    }
+}
