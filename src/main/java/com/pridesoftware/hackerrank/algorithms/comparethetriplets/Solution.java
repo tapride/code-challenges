86803131067a779ef58f123729e4e901312b9594
Tom Pride <PrideThomasA@PrideFamily.us>
2017-08-16 18:25:59 -0500

    Submission 1 - Passed

--- src/main/java/com/pridesoftware/hackerrank/algorithms/comparethetriplets/Solution.java
+++ src/main/java/com/pridesoftware/hackerrank/algorithms/comparethetriplets/Solution.java
@@ -1,15 +1,22 @@
 package com.pridesoftware.hackerrank.algorithms.comparethetriplets;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
 public class Solution {
 
     static int[] solve(int a0, int a1, int a2, int b0, int b1, int b2){
         // Complete this function
-        List<Integer> values = new ArrayList<Integer>();
-        return new int[]{0, 1};
+        return solve(new int[]{a0,a1,a2}, new int[]{b0,b1,b2}, 3);
+    }
+
+    static int[] solve(int[] a, int[] b, int length){
+        int[] score = new int[]{0,0};
+
+        for (int i = 0; i < length; i++){
+            if (a[i] > b[i]) score[0]++;
+            else if (b[i] > a[i]) score[1]++;
+        }
+        return score;
     }
 
     public static void main(String[] args) {
