a17154be56ad9849f82b54707a33fa1c508dfdfb
Tom Pride <PrideThomasA@PrideFamily.us>
2017-08-16 19:01:01 -0500

    Added Class Framework

---
+++ src/main/java/com/pridesoftware/hackerrank/algorithms/aVeryBigSum/Solution.java
@@ -0,0 +1,22 @@
+package com.pridesoftware.hackerrank.algorithms.aVeryBigSum;
+
+import java.util.Scanner;
+
+public class Solution {
+
+    static long aVeryBigSum(int n, long[] ar) {
+        // Complete this function
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        long[] ar = new long[n];
+        for(int ar_i = 0; ar_i < n; ar_i++){
+            ar[ar_i] = in.nextLong();
+        }
+        long result = aVeryBigSum(n, ar);
+        System.out.println(result);
+    }
+}
